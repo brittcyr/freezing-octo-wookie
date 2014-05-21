fcead91044a70a0c1215ffcb846d4a382ade04f8
Britt Cyr <cyrbritt@mit.edu>
2014-05-21 14:40:59 -0400

    adding better redirection

--- scripts/get_faceoffs.py
+++ scripts/get_faceoffs.py
@@ -18,13 +18,18 @@ def get_faces(url):
   try:
     html_parser = HTMLParser.HTMLParser()
     br = feedparser.parse(url + '?view=plays')
-    br = feedparser.parse(br['href'] + '?view=plays')
+    current_page = br['href']
+    if '?view=plays' not in current_page:
+      current_page += '?view=plays'
+    else:
+      current_page = current_page.split('?view=plays')[0] + '?view=plays'
+    br = feedparser.parse(current_page)
     statboxes = BeautifulSoup(str(br)).findAll("div", {"class" :"stats-fullbox clearfix"})
     statbox = statboxes[-1]
 
     # In case there are multiple statboxes on the page
     for box in statboxes:
-      if 'Face' in str(box):
+      if 'Face' in str(box) and 'won by' in str(box):
         statbox = box
         break
 
@@ -143,8 +148,8 @@ def get_faces_other_type(url):
 
 if __name__ == '__main__':
   get_faces('http://www.generalssports.com/sports/mlax/2013-14/boxscores/20140329_b213.xml?view=plays')
-  get_faces('http://www.mitchellathletics.com/sports/mlax/2013-14/boxscores/20140412_v9l5.xml?view=undefined')
-  get_faces('http://www.ritathletics.com/boxscore.aspx?id=7227&path=mlax')
-  get_faces('http://www.laxmagazine.com/links/b9gzz2')
+  #get_faces('http://www.mitchellathletics.com/sports/mlax/2013-14/boxscores/20140412_v9l5.xml?view=undefined')
+  #get_faces('http://www.ritathletics.com/boxscore.aspx?id=7227&path=mlax')
+  #get_faces('http://www.laxmagazine.com/links/b9gzz2')
   pass
   
