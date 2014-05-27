b02b1d82cbbf24d3fa1abe0a1ff70ea3e51e0567
cyrbritt <cyrbritt@whole-enchilada.mit.edu>
2014-05-27 19:35:36 -0400

    starting to make a model

--- fogo/views.py
+++ fogo/views.py
@@ -1,1 +1,16 @@
 # Create your views here.
+from fogo.models import Player, Game, Ref, Faceoff
+from django.template import Context, loader, RequestContext
+from django.http import HttpResponse
+from django.shortcuts import render_to_response, redirect
+from django.db.models import Sum
+
+
+def index(request):
+    faceoffs = Faceoff.objects.all()[:100]
+
+    return render_to_response('index.html',
+                              {
+                                'faceoffs': faceoffs,
+                              },
+                              context_instance=RequestContext(request))
--- fogolytics/settings.py
+++ fogolytics/settings.py
@@ -109,9 +109,7 @@ ROOT_URLCONF = 'fogolytics.urls'
 WSGI_APPLICATION = 'fogolytics.wsgi.application'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.dirname(__file__) , '../templates').replace('\\','/')
 )
 
 INSTALLED_APPS = (
