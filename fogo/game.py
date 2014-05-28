c1bf5f0d32476b0e6b330741ec3d34914832eb39
Britt Cyr <cyrbritt@mit.edu>
2014-05-28 12:37:37 -0400

    adding info to the game page

--- fogo/game.py
+++ fogo/game.py
@@ -4,9 +4,10 @@ from django.template import Context, loader, RequestContext
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, redirect
 from django.db.models import Sum
+import datetime
 
 def index(request, game):
-    game = Game.objects.filter(id=game)
+    game = Game.objects.filter(id=game)[0]
     faceoffs = Faceoff.objects.filter(game=game)
 
     return render_to_response('game.html',
