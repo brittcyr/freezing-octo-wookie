46b2cf4245289d6b3b3a091e72c80407baa5e983
Britt Cyr <cyrbritt@mit.edu>
2014-06-09 08:08:51 -0400

    remove imports

--- fogo/views.py
+++ fogo/views.py
@@ -1,12 +1,9 @@
 # Create your views here.
-from fogo.models import Player, Game, Ref, Faceoff
+from fogo.models import Faceoff
 from django.template import Context, loader, RequestContext
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, redirect
-from django.db.models import Sum
-
 
 def index(request):
-    faceoffs = Faceoff.objects.all()[:100]
-
+    faceoffs = Faceoff.objects.all()[:10]
     return str(faceoffs)
