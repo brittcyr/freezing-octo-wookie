751aeb3281f1b1a833a0f36631319bb919211dc1
Britt Cyr <cyrbritt@mit.edu>
2014-06-15 20:30:17 -0400

    typeahead links on select

--- fogo/search.py
+++ fogo/search.py
@@ -8,13 +8,11 @@ def query(request):
     query = request.GET.get('query', '')
     if len(query) > 0:
         results = Player.objects.filter(name__istartswith=query)
-        results_list = [x.name for x in results]
-    else:
-        results_list = []
 
     response = []
-    for r in results_list:
-      response.append({'value':r})
+    for r in results:
+      link = '/fogolytics/player/' + str(r.id)
+      response.append({'value':r.name, 'link':link})
     response_text = json.dumps(response)
 
     return HttpResponse(response_text, content_type="application/json")
