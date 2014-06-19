51a2d91b0375f70133625b3354c1913c3c585c7e
Britt Cyr <cyrbritt@mit.edu>
2014-06-18 21:25:45 -0400

    all players load data for qualified players

--- fogo/all_players.py
+++ fogo/all_players.py
@@ -1,5 +1,5 @@
 # Create your views here.
-from fogo.models import Player, Faceoff
+from fogo.models import Player, Faceoff, Team, Game
 from django.template import Context, loader, RequestContext
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
@@ -34,6 +34,23 @@ def index(request):
         player.wins = face_wins[player.id]
         player.num_taken = face_taken[player.id]
         player.percent = float(player.wins) / player.num_taken * 100.0
+
+    teams = Team.objects.all()
+    home_faceoffs = Game.objects.values('home') \
+                                 .annotate(wins=Sum('home_wins')) \
+                                 .annotate(total=Sum('total_face'))
+    away_faceoffs = Game.objects.values('away') \
+                                 .annotate(wins=Sum('home_wins')) \
+                                 .annotate(total=Sum('total_face'))
+
+    team_faces = [0]*(len(teams)*2)
+    for face in away_faceoffs:
+        team_faces[face['away']] = face['total']
+    for face in home_faceoffs:
+        team_faces[face['home']] += face['total']
+
+    players = list(players)
+    players = [p for p in players if (p.num_taken > .4 * team_faces[p.team.id] and p.num_taken > 50)]
 
     return render_to_response('all_players.html',
                               {
--- fogo/player.py
+++ fogo/player.py
@@ -3,7 +3,6 @@ from fogo.models import Player, Game, Ref, Faceoff
 from django.template import Context, loader, RequestContext
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
-import datetime
 from django.db.models import Sum, Count
 
 def index(request, player):
